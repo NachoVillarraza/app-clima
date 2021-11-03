2ee7555be9bfa2b012aa7fd8721ead86d35ae2b5
Nacho <nvillarraza@gmail.com>
2021-11-03 10:49:45 -0300

    Proyecto Clima Curso Udemy Finalizado y Funcionando

---
+++ JS/app.js
@@ -0,0 +1,67 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+
+  // const cityDets = data.cityDets;
+  // const weather = data.weather;
+
+  const { cityDets, weather } = data;
+
+  console.log(data);
+
+  details.innerHTML = `
+    <h5 class="my-5">${cityDets.EnglishName}</h5>
+        <div class="my-5">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+          <span>${weather.Temperature.Metric.Value}</span>
+          <span>&deg;</span>
+        </div>
+  
+  `
+
+  let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src',  timeSrc);
+
+  if(card.classList.contains('d-none')){
+    card.classList.remove('d-none')
+  }
+
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+
+}; 
+
+const updateCity = async (city) => {
+
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+  return { cityDets, weather };
+
+};
+
+cityForm.addEventListener('submit', e => {
+  // prevent default action
+  e.preventDefault();
+  
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update the ui with new city
+  updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+
+  localStorage.setItem('city', city);
+});
+
+
+if(localStorage.getItem('city')){
+  updateCity(localStorage.getItem('city'))
+  .then(data => updateUI(data))
+  .catch(err => console.log(err));
+};
